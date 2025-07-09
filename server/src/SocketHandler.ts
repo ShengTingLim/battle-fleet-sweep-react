cb9cceb699843751036ffdc5de3ad29692dd63ae
Sheng Ting Lim <sl774@students.waikato.ac.nz>
2025-07-09 20:01:53 +1200

    Server SocketHandler.ts: Add event handlers for socket disconnect, createLobby, and joinLobby

--- server/src/SocketHandler.ts
+++ server/src/SocketHandler.ts
@@ -1,5 +1,6 @@
 import { Server, Socket } from 'socket.io';
 import { User } from './User';
+import { LobbyHandler } from './LobbyHandler';
 
 const sockets = new Map<string, SocketHandler>();
 
@@ -14,5 +15,26 @@ export class SocketHandler {
         this.user = new User(socket.id);
 
         sockets.set(socket.id, this);
+
+        this.socket.on('disconnect', () => {
+            console.log(`Client disconnected: ${this.socket.id}`);
+            this.user.deleteUser();
+            sockets.delete(this.socket.id);
+        });
+
+        this.socket.on('createLobby', (username: string) => {
+            if(!this.user.setUsername(username)) {
+                
+            }
+            const lobby = new LobbyHandler(this.user);
+            const lobbyId = lobby.getLobbyId();
+            console.log(`User ${username} created a lobby with ID: ${lobbyId}`);
+            this.socket.join(lobbyId);
+            this.io.to(this.socket.id).emit('lobbyCreated', this.socket.id);
+        })
+
+        this.socket.on('joinLobby', (username: string, lobbyId: string) => {
+
+        })
     }
 }
