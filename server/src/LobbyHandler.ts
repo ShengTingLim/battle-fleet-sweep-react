f0fa07e47330744dd197ac87b8bd79454cf44fdb
Sheng Ting Lim <sl774@students.waikato.ac.nz>
2025-07-09 20:01:02 +1200

    Server LobbyHandler.ts: Create inital class and add some basic methods

---
+++ server/src/LobbyHandler.ts
@@ -0,0 +1,38 @@
+import { User } from './User';
+import { generateGameCode } from './utils';
+
+const lobbies = new Map<string, LobbyHandler>();
+
+export class LobbyHandler {
+    private lobbyId: string;
+    private user1: User | null = null;
+    private user2: User | null = null;
+
+    constructor(User1: User) {
+        while (!LobbyHandler.getLobbyById(this.lobbyId = generateGameCode())) {
+            lobbies.set(this.lobbyId, this);
+        }
+        this.user1 = User1;
+    }
+
+    static getLobbyById(lobbyId: string): LobbyHandler | undefined {
+        return lobbies.get(lobbyId);
+    }
+
+    getLobbyId(): string {
+        return this.lobbyId;
+    }
+
+    joinLobby(user: User): boolean {
+        if (this.user2) {
+            return false;
+        }
+        if (user.getGameId()) {
+            return false;
+        }
+        this.user2 = user;
+        user.joinGame(this.lobbyId);
+        return true;
+        
+    }
+}
